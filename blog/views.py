11332e697bbd4812d5970c92cd754921849f3f30
Anastasiia Lazorenko <lazorenko@ucu.edu.ua>
2018-04-29 15:48:57 +0300

    Used template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post # Comment, Reaction
+from django.contrib.auth.models import User
+
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    suser = User.objects.get(username='SuperU')
+    posts = Post.objects.filter(author=suser).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
